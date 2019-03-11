056714e3000920bb530b566c11ccde09354fed53
Miguel Rentes <it@lusocal.com>
2019-03-11 12:17:20 +0000

    Adds example for Core Data Types: Numbers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python3-code-snippets
-Python 3 Code Snippets
+Python 3 Code Snippets :snake:
+
+This is a simple project where I share Python 3 code snippets to perform everyday programming tasks.
+To run these code snippets all that is required is a Python 3 interpreter.
---
+++ core_datatypes/numbers/numbers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Python 3 examples using Numbers (https://github.com/rentes/python3-code-snippets)
+    Author: Miguel Rentes
+    Python version: 3.7.2
+"""
+result = 123 + 456 # Integer Addition
+print(result) # Result: 579
+print(type(result)) # result is an object of type 'int'
+result = 1.23 + 456 # Float Addition
+print(result) # Result: 457.23
+print(type(result)) # result is an object of type 'float'
+result = 123 - 456 # Integer Subtraction
+print(result) # Result: -333
+print(type(result)) # result is an object of type 'int'
+result = 1.23 - 456 # Float Subtraction
+print(result) # Result: -454.77
+print(type(result)) # result is an object of type 'float'
+result = 123 * 456 # Integer Multiplication
+print(result) # Result: 56088
+print(type(result)) # result is an object of type 'int'
+result = 123 / 456 # Integer Division
+print(result) # Result: 0.26973684210526316
+print(type(result)) # result is an object of type 'float'
+result = 123 ** 2 # Integer Exponentiation, e.g., successive multiplications. 123 ** 2 = 123 * 123
+print(result) # Result: 15129
+print(type(result)) # result is an object of type 'int'
+result = 1.23 ** 2 # Float Exponentiation
+print(result) # Result: 1.5129
+print(type(result)) # result is an object of type 'float'
