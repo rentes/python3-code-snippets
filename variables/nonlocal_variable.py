e92522e2d57c132035d0e843b7832300033dd419
Miguel Rentes <it@lusocal.com>
2019-05-13 15:06:26 +0100

    Corrects docstring for the nonlocal variable example

--- variables/nonlocal_variable.py
+++ variables/nonlocal_variable.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-    Global vs Local variables
+    Nonlocal variables
     Author: Miguel Rentes
-    github.com/rentes/python3-code-snippets/blob/master/variables/nonlocal_variable.py
+    https://github.com/rentes/python3-code-snippets/blob/master/variables/nonlocal_variable.py
     Python version: 3.7.2
     A nonlocal variable lets you assign values to a variable in an outer (but non-global) scope
+    Check PEP 3104 for more details - https://www.python.org/dev/peps/pep-3104/
 """
 def F(first_parameter, second_parameter):
     """ 
